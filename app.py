e02c8f7f073309c8ef700f96d563e3433176f7cc
tfoley3 <timmythetimster@hotmail.com>
2026-06-30 10:59:20 +0100

    Add password gate to protect against unauthorised API usage

--- app.py
+++ app.py
@@ -65,6 +65,29 @@ def summarise_document(extracted_text):
 
 ####################################### Set up page config ###################################################
 
+# password gate to prevent unauthorised API usage
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == os.getenv("APP_PASSWORD"):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't keep password in memory
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Enter password to access this demo", type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input("Enter password to access this demo", type="password", on_change=password_entered, key="password")
+        st.error("Incorrect password")
+        return False
+    else:
+        return True
+
+if not check_password():
+    st.stop()  # halt execution until correct password is entered
+
+
 st.set_page_config(
     page_title="Document Intelligence App",  # sets the browser tab title
     page_icon="📄",  # sets the browser tab icon
